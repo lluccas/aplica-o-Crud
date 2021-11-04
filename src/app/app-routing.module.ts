210cb8a9f824ebb59c0068262ff32bf374eecf9c
Luccas Torres <luccastorresbuzz@gmail.com>
2021-11-03 22:38:11 -0300

    atualizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductCreatComponent } from './components/product/product-creat/product-creat.component';
+import {HomeComponent} from './views/home/home.component';
+import {ProductCrudComponent} from './views/product-crud/product-crud.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:"",
+  component: HomeComponent
+},{
+  path:"products",
+  component: ProductCrudComponent
+},{
+  path: "products/creat",
+  component: ProductCreatComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
